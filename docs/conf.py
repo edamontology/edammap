e7c2a7e7791dd16712f364d9df8e86890c20f46f
Erik Jaaniso <egj@gmx.fr>
2022-02-03 16:33:35 +0200

    Start 1.1.1-SNAPSHOT

--- docs/conf.py
+++ docs/conf.py
@@ -4,8 +4,8 @@
 project = 'EDAMmap'
 author = 'Erik Jaaniso'
 copyright = '2016-2020, Erik Jaaniso'
-version = '1.1.0-SNAPSHOT'
-release = '1.1.0-SNAPSHOT'
+version = '1.1.1-SNAPSHOT'
+release = '1.1.1-SNAPSHOT'
 
 master_doc = 'index'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
